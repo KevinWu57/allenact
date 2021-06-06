298e5ed745a8f294b5d55d807c49602bf04785cf
KevinWu57 <qiwu57kevin@gmail.com>
2021-06-05 23:52:45 -0700

    Add new actor-critic model: ResnetTensorObjectGestureNavActorCritic

--- projects/objectnav_baselines/models/object_nav_models.py
+++ projects/objectnav_baselines/models/object_nav_models.py
@@ -755,3 +755,121 @@ class ResnetDualTensorGoalGestureEncoder(nn.Module):
         x = x.reshape(x.size(0), -1)  # flatten 
 
         return self.adapt_output(x, use_agent, nstep, nsampler, nagent)
+    
+class ResnetTensorObjectGestureNavActorCritic(ActorCriticModel[CategoricalDistr]):
+    def __init__(
+        self,
+        action_space: gym.spaces.Discrete,
+        observation_space: SpaceDict,
+        goal_sensor_uuid: str,
+        gesture_sensor_uuid: Optional[str],
+        human_pose_uuid: Optional[str],
+        rgb_resnet_preprocessor_uuid: Optional[str],
+        depth_resnet_preprocessor_uuid: Optional[str] = None,
+        hidden_size: int = 512,
+        goal_dims: int = 32,
+        gesture_compressor_hidden_out_dim: int = 128,
+        resnet_compressor_hidden_out_dims: Tuple[int, int] = (128, 32),
+        combiner_hidden_out_dims: Tuple[int, int] = (128, 32),
+        include_auxiliary_head: bool = False,
+    ):
+
+        super().__init__(
+            action_space=action_space, observation_space=observation_space,
+        )
+
+        self._hidden_size = hidden_size
+        self.include_auxiliary_head = include_auxiliary_head
+        if (
+            rgb_resnet_preprocessor_uuid is None
+            or depth_resnet_preprocessor_uuid is None
+        ):
+            resnet_preprocessor_uuid = (
+                rgb_resnet_preprocessor_uuid
+                if rgb_resnet_preprocessor_uuid is not None
+                else depth_resnet_preprocessor_uuid
+            )
+            self.goal_visual_encoder = ResnetTensorGoalEncoder(
+                self.observation_space,
+                goal_sensor_uuid,
+                resnet_preprocessor_uuid,
+                goal_dims,
+                resnet_compressor_hidden_out_dims,
+                combiner_hidden_out_dims,
+            )
+        else:
+            self.goal_visual_encoder = ResnetDualTensorGoalGestureEncoder(  # type:ignore
+                self.observation_space,
+                goal_sensor_uuid,
+                gesture_sensor_uuid,
+                human_pose_uuid,
+                rgb_resnet_preprocessor_uuid,
+                depth_resnet_preprocessor_uuid,
+                goal_dims,
+                gesture_compressor_hidden_out_dim,
+                resnet_compressor_hidden_out_dims,
+                combiner_hidden_out_dims,
+            )
+        self.state_encoder = RNNStateEncoder(
+            self.goal_visual_encoder.output_dims, self._hidden_size,
+        )
+        self.actor = LinearActorHead(self._hidden_size, action_space.n)
+        self.critic = LinearCriticHead(self._hidden_size)
+        if self.include_auxiliary_head:
+            self.auxiliary_actor = LinearActorHead(self._hidden_size, action_space.n)
+
+        self.train()
+        
+    @property
+    def recurrent_hidden_state_size(self) -> int:
+        """The recurrent hidden state size of the model."""
+        return self._hidden_size
+
+    @property
+    def is_blind(self) -> bool:
+        """True if the model is blind (e.g. neither 'depth' or 'rgb' is an
+        input observation type)."""
+        return self.goal_visual_encoder.is_blind
+
+    @property
+    def num_recurrent_layers(self) -> int:
+        """Number of recurrent hidden layers."""
+        return self.state_encoder.num_recurrent_layers
+
+    def _recurrent_memory_specification(self):
+        return dict(
+            rnn=(
+                (
+                    ("layer", self.num_recurrent_layers),
+                    ("sampler", None),
+                    ("hidden", self.recurrent_hidden_state_size),
+                ),
+                torch.float32,
+            )
+        )
+
+    def get_object_type_encoding(
+        self, observations: Dict[str, torch.FloatTensor]
+    ) -> torch.FloatTensor:
+        """Get the object type encoding from input batched observations."""
+        return self.goal_visual_encoder.get_object_type_encoding(observations)
+
+    def forward(  # type:ignore
+        self,
+        observations: ObservationType,
+        memory: Memory,
+        prev_actions: torch.Tensor,
+        masks: torch.FloatTensor,
+    ) -> Tuple[ActorCriticOutput[DistributionType], Optional[Memory]]:
+        x = self.goal_visual_encoder(observations)
+        x, rnn_hidden_states = self.state_encoder(x, memory.tensor("rnn"), masks)
+        return (
+            ActorCriticOutput(
+                distributions=self.actor(x),
+                values=self.critic(x),
+                extras={"auxiliary_distributions": self.auxiliary_actor(x)}
+                if self.include_auxiliary_head
+                else {},
+            ),
+            memory.set_tensor("rnn", rnn_hidden_states),
+        )
